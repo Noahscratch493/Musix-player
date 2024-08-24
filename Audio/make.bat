236c754484a3ab4d30bfa46b13b1817fcdf9fa0b
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2024-08-24 16:21:03 +0100

    Delete Audio/make.bat

--- Audio/make.bat
+++
@@ -1,4 +0,0 @@
-gcc -c -o "C:\Dev-Cpp\OS Creator\0.2\templates\newproj\kernel.o" "C:\Dev-Cpp\OS Creator\0.2\templates\newproj\kernel.c" 
-nasm -f win32 -o "C:\Dev-Cpp\OS Creator\0.2\templates\newproj\asmkernel.o" "C:\Dev-Cpp\OS Creator\0.2\templates\newproj\kernel.asm"
-ld -o "C:\Dev-Cpp\OS Creator\0.2\templates\newproj\kernel.bin" "C:\Dev-Cpp\OS Creator\0.2\templates\newproj\kernel.o" "C:\Dev-Cpp\OS Creator\0.2\templates\newproj\asmkernel.o" 
-
